0ff0b2463f33b84b1f884d8e36eca06bcb226cec
MWDelaney <michael@michaeldelaney.me>
2023-09-12 10:15:40 -0400

    Frequency

--- _data/sites/clients.js
+++ _data/sites/clients.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Clients", // optional, falls back to object key
 	description: "Rebel's client sites",
 	options: {
-		frequency: 60 * 23, // 24 hours
+		frequency: 1,
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
